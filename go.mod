d1e9dab1183e393f0b2b4a0a93408a2a4a87e01a
GKOMPE-BLRM20 <gkompe-blrm20@GKOMPE-BLRM20s-MacBook-Pro.local>
2022-04-22 14:41:23 +0530

    MUS-49-callback lambda

--- go.mod
+++ go.mod
@@ -7,4 +7,31 @@ require (
 	github.com/aws/aws-sdk-go v1.43.43
 	github.eagleview.com/engineering/assess-platform-library v0.0.0-20220407073828-18351a5be313
 	github.eagleview.com/engineering/platform-gosdk v1.1.3
+	go.mongodb.org/mongo-driver v1.9.0
+)
+
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- lambdas/callbacklambda/main.go
+++ lambdas/callbacklambda/main.go
@@ -1,10 +1,87 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"log"
+	"strconv"
+	"time"
+
 	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sfn"
+	"github.eagleview.com/engineering/symphony-service/commons/aws_client"
+	"github.eagleview.com/engineering/symphony-service/commons/documentDB_client"
 )
 
-func main() {
-	fmt.Println("Generic Callout lambda modlule")
+var awsClient aws_client.AWSClient
 
+const Success = "success"
+
+type RequestBody struct {
+	Status      string                 `json:"status"`
+	Message     string                 `json:"message"`
+	MessageCode int                    `json:"messageCode"`
+	CallbackID  string                 `json:"callbackId"`
+	Response    map[string]interface{} `json:"response"`
+}
+
+var SecretARN string
+
+func Handler(ctx context.Context, CallbackRequest map[string]interface{}) (map[string]interface{}, error) {
+	secrets, err := awsClient.GetSecret(context.Background(), SecretARN, "us-east-2")
+	documentDB_client.Username = secrets["username"].(string)
+	documentDB_client.Password = secrets["password"].(string)
+	documentDB_client.ClusterEndpoint = fmt.Sprintf("%v:%v", secrets["host"], secrets["port"])
+	connectionURI := fmt.Sprintf(documentDB_client.ConnectionStringTemplate, documentDB_client.Username, documentDB_client.Password, documentDB_client.ClusterEndpoint, documentDB_client.ReadPreference)
+	NewDBClient := documentDB_client.NewDBClientService(documentDB_client.CaFilePath, connectionURI)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	documentDB_client.DBClient = NewDBClient.DBClient
+	err = documentDB_client.DBClient.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mySession := session.Must(session.NewSession())
+	svc := sfn.New(mySession)
+	body := CallbackRequest["body"].(string)
+	var requestBody = RequestBody{}
+	err = json.Unmarshal([]byte(body), &requestBody)
+	if err != nil {
+		return map[string]interface{}{"status": "failed"}, err
+	}
+	MetaData, err := NewDBClient.FetchMetaData(requestBody.CallbackID)
+	if err != nil {
+		return map[string]interface{}{"status": "failed"}, err
+	}
+	if requestBody.Status == Success {
+		byteData, _ := json.Marshal(requestBody.Response)
+		jsonResponse := string(byteData)
+		taskoutput, err := svc.SendTaskSuccess(&sfn.SendTaskSuccessInput{
+			TaskToken: &MetaData.Data.TaskToken,
+			Output:    &jsonResponse,
+		})
+		fmt.Println(&taskoutput, err)
+	} else {
+		messageCode := strconv.Itoa(requestBody.MessageCode)
+		taskoutput, err := svc.SendTaskFailure(&sfn.SendTaskFailureInput{
+			TaskToken: &MetaData.Data.TaskToken,
+			Cause:     &requestBody.Message,
+			Error:     &messageCode,
+		})
+		fmt.Println(&taskoutput, err)
+	}
+	err = NewDBClient.DeleteMetaData(requestBody.CallbackID)
+	if err != nil {
+		return map[string]interface{}{"status": "failed"}, err
+	}
+	return map[string]interface{}{"status": Success}, nil
+}
+
+func main() {
+
+	lambda.Start(Handler)
 }
