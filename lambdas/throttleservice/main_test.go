166288beeac2b745d581d5170cd9a52af590682c
Piyush Tyagi <piyush.tyagi@eagleview.com>
2022-05-18 16:07:41 +0530

    MUS-17 updated test cases

--- lambdas/throttleservice/main_test.go
+++ lambdas/throttleservice/main_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.eagleview.com/engineering/symphony-service/commons/log_config"
 	"github.eagleview.com/engineering/symphony-service/commons/mocks"
 )
 
@@ -16,6 +17,8 @@ var eventTestData string = `{
 	"orderId": "44825849",
 	"workflowId": "9cabffdf-e980-0bbf-b481-0048f7a88bef"
   }`
+
+var testContext = log_config.SetTraceIdInContext(context.Background(), "44825849", "9cabffdf-e980-0bbf-b481-0048f7a88bef")
 
 func TestThrottleLambdaTwisterFlow(t *testing.T) {
 	dBClient := new(mocks.IDocDBClient)
@@ -28,10 +31,10 @@ func TestThrottleLambdaTwisterFlow(t *testing.T) {
 	commonHandler.DBClient = dBClient
 	var count int64 = 52
 	t.Setenv("AllowedHipsterCount", "50")
-	dBClient.Mock.On("GetHipsterCountPerDay", context.Background()).Return(count, nil)
-	dBClient.Mock.On("UpdateDocumentDB", context.Background(), mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	dBClient.Mock.On("GetHipsterCountPerDay", testContext).Return(count, nil)
+	dBClient.Mock.On("UpdateDocumentDB", testContext, mock.Anything, mock.Anything, mock.Anything).Return(nil)
 
-	resp, err := handler(context.Background(), eventDataRequestObj)
+	resp, err := notifcationWrapper(context.Background(), eventDataRequestObj)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedResp, resp)
 
@@ -39,38 +42,40 @@ func TestThrottleLambdaTwisterFlow(t *testing.T) {
 
 func TestThrottleLambdaGetHipsterCountPerDayError(t *testing.T) {
 	dBClient := new(mocks.IDocDBClient)
-
+	slackclient := new(mocks.ISlackClient)
 	eventDataRequestObj := &eventData{}
 	mydata := []byte(eventTestData)
 	json.Unmarshal(mydata, &eventDataRequestObj)
 
 	expectedResp := map[string]interface{}{"status": failed}
 	commonHandler.DBClient = dBClient
+	commonHandler.SlackClient = slackclient
 	var count int64 = 52
 	t.Setenv("AllowedHipsterCount", "50")
-	dBClient.Mock.On("GetHipsterCountPerDay", context.Background()).Return(count, errors.New("some error"))
-	dBClient.Mock.On("UpdateDocumentDB", context.Background(), mock.Anything, mock.Anything, mock.Anything).Return(nil)
-
-	resp, err := handler(context.Background(), eventDataRequestObj)
+	dBClient.Mock.On("GetHipsterCountPerDay", testContext).Return(count, errors.New("some error"))
+	dBClient.Mock.On("UpdateDocumentDB", testContext, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	slackclient.Mock.On("SendErrorMessage", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	resp, err := notifcationWrapper(context.Background(), eventDataRequestObj)
 	assert.Error(t, err)
 	assert.Equal(t, expectedResp, resp)
 
 }
 func TestThrottleLambdaErrorStringParse(t *testing.T) {
 	dBClient := new(mocks.IDocDBClient)
-
+	slackclient := new(mocks.ISlackClient)
 	eventDataRequestObj := &eventData{}
 	mydata := []byte(eventTestData)
 	json.Unmarshal(mydata, &eventDataRequestObj)
 
 	expectedResp := map[string]interface{}{"status": failed}
 	commonHandler.DBClient = dBClient
+	commonHandler.SlackClient = slackclient
 	var count int64 = 52
 	t.Setenv("AllowedHipsterCount", "abc")
-	dBClient.Mock.On("GetHipsterCountPerDay", context.Background()).Return(count, nil)
-	dBClient.Mock.On("UpdateDocumentDB", context.Background(), mock.Anything, mock.Anything, mock.Anything).Return(errors.New("some error"))
-
-	resp, err := handler(context.Background(), eventDataRequestObj)
+	dBClient.Mock.On("GetHipsterCountPerDay", testContext).Return(count, nil)
+	dBClient.Mock.On("UpdateDocumentDB", testContext, mock.Anything, mock.Anything, mock.Anything).Return(errors.New("some error"))
+	slackclient.Mock.On("SendErrorMessage", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	resp, err := notifcationWrapper(context.Background(), eventDataRequestObj)
 	assert.Error(t, err)
 	assert.Equal(t, expectedResp, resp)
 
@@ -78,19 +83,20 @@ func TestThrottleLambdaErrorStringParse(t *testing.T) {
 
 func TestThrottleLambdaUpdateDocumentDBError(t *testing.T) {
 	dBClient := new(mocks.IDocDBClient)
-
+	slackclient := new(mocks.ISlackClient)
 	eventDataRequestObj := &eventData{}
 	mydata := []byte(eventTestData)
 	json.Unmarshal(mydata, &eventDataRequestObj)
 
 	expectedResp := map[string]interface{}{"status": failed}
 	commonHandler.DBClient = dBClient
+	commonHandler.SlackClient = slackclient
 	var count int64 = 52
 	t.Setenv("AllowedHipsterCount", "50")
-	dBClient.Mock.On("GetHipsterCountPerDay", context.Background()).Return(count, nil)
-	dBClient.Mock.On("UpdateDocumentDB", context.Background(), mock.Anything, mock.Anything, mock.Anything).Return(errors.New("some error"))
-
-	resp, err := handler(context.Background(), eventDataRequestObj)
+	dBClient.Mock.On("GetHipsterCountPerDay", testContext).Return(count, nil)
+	dBClient.Mock.On("UpdateDocumentDB", testContext, mock.Anything, mock.Anything, mock.Anything).Return(errors.New("some error"))
+	slackclient.Mock.On("SendErrorMessage", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	resp, err := notifcationWrapper(context.Background(), eventDataRequestObj)
 	assert.Error(t, err)
 	assert.Equal(t, expectedResp, resp)
 
