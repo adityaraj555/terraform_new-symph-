292fd431861b7ce9eec95ed837226223cbbe27b8
Shashikant Kurhade <shashikant.kurhade@eagleview.com>
2022-05-25 15:44:16 +0530

    temp changes

--- terraform/symphony/roles.tf
+++ terraform/symphony/roles.tf
@@ -55,12 +55,27 @@ POLICY
 
 
 resource "aws_iam_role" "platform-data-orchestrator-callback-lambda-s3" {
-  assume_role_policy = module.config.environment_config_map.trust_relashionships_external_service
+  assume_role_policy = <<POLICY
+{
+   "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "${module.config.environment_config_map.cross_account_callback_lambda}"
+                ]
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
 
   inline_policy {
     name   = "platform-data-orchestrator-resources-access-policy"
     policy = <<POLICY
-    {
+{
         "Version": "2012-10-17",
         "Statement": [
             {
