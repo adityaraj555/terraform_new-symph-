be76fa35ae69acfe47bdc4eaddfa2d533bcc4b2d
Shashikant Kurhade <shashikant.kurhade@eagleview.com>
2022-09-07 19:06:07 +0530

    Testing service account roe

--- terraform/symphony/roles.tf
+++ terraform/symphony/roles.tf
@@ -52,7 +52,60 @@ POLICY
 }
 
 
+resource "aws_iam_role" "platform-data-orchestrator-service-role" {
+  assume_role_policy = <<POLICY
+${module.config.environment_config_map.trust_relashionships_external_service_factory_dx}
+  POLICY
+  inline_policy {
+    name   = "platform-data-orchestrator-service-role-access-policy"
+    policy = <<POLICY
+{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": [
+                    "lambda:InvokeFunction",
+                    "lambda:InvokeAsync",
+                    "ec2:DescribeInstances",
+                    "ec2:DescribeInstanceStatus",
+                    "ec2:DeleteTags",
+                    "ec2:CreateTags",
+                    "s3:PutObject",
+                    "s3:PutObjectAcl",
+                    "s3:DeleteObject",
+                    "s3:GetObject",
+                    "s3:GetObjectAcl",
+                    "sqs:DeleteMessage",
+                    "sqs:ReceiveMessage",
+                    "sqs:GetQueueAttributes"
+                ],
+                "Resource": [
+                    "arn:aws:lambda:${local.region}:${local.account_id}:function:${local.resource_name_prefix}-lambda-${module.config.environment_config_map.callback_lambda_name}",
+                    "arn:aws:s3:::${local.resource_name_prefix}-s3-property-data-orchestrator",
+                    "arn:aws:s3:::${local.resource_name_prefix}-s3-property-data-orchestrator/*"
+                    "arn:aws:sqs:${local.region}:${local.account_id}:${local.resource_name_prefix}-sqs-receiveLegacyOrder"
 
+                ],
+                "Effect": "Allow",
+                "Sid": "AccessCallback2"
+            }
+        ]
+    }
+    POLICY
+    
+  }
+
+  managed_policy_arns = []
+
+  max_session_duration = "3600"
+  name                 = "${local.resource_name_prefix}-role-pdo-access"
+  path                 = "/"
+
+  tags = {
+    Name        = "${local.resource_name_prefix}-role-pdo-access"
+    Description = "AWS IAM role to allow services to access platform-data-orchestrator common resources like s3 and callback Lambda"
+  }
+}
 
 resource "aws_iam_role" "platform-data-orchestrator-callback-lambda-s3" {
   assume_role_policy = <<POLICY
