10d3faac40210470a927c86d2c5bff19509760e3
T Varun <t.varun@eagleview.com>
2022-09-26 16:20:49 +0530

    [DO NOT MERGE] MUS-583 : geocoding integration (#169)
    * tmp changes
    
    * MUS-583 : using geocoding service
    
    * MUS-583 : removed code
    
    * MUS-583: corrected error

--- lambdas/querypdw/main_test.go
+++ lambdas/querypdw/main_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.eagleview.com/engineering/symphony-service/commons/error_handler"
 	"github.eagleview.com/engineering/symphony-service/commons/mocks"
 )
 
@@ -59,8 +60,10 @@ func TestHandlerTriggerSIM(t *testing.T) {
 					  "id": "5085a802-89fa-48a8-8c3c-bd8480f0378a"
 					}
 				  ],
-				  "lat": 43.172988,
-				  "lon": -77.501957,
+				  "geocoder": {
+					  "lat": 43.172988,
+					  "lon": -77.501957
+				  },
 				  "address": "23 HAVENSHIRE RD",
 				  "city": "ROCHESTER"
 				}
@@ -69,13 +72,14 @@ func TestHandlerTriggerSIM(t *testing.T) {
 		  }`))),
 		StatusCode: http.StatusOK,
 	}, nil)
+	http_Client.On("Get").Return(nil, nil)
 	commonHandler.AwsClient = aws_Client
 	commonHandler.HttpClient = http_Client
 	commonHandler.Secrets = map[string]interface{}{
 		"ClientID":     "id",
 		"ClientSecret": "secret"}
 	expectedResp := eventResponse{
-		Address:    "23 HAVENSHIRE RD ROCHESTER NY 14625",
+		Address:    "23 HAVENSHIRE RD, ROCHESTER, NY, 14625",
 		Latitude:   43.172988,
 		Longitude:  -77.501957,
 		TriggerSIM: true,
@@ -87,6 +91,276 @@ func TestHandlerTriggerSIM(t *testing.T) {
 	assert.Equal(t, expectedResp, resp)
 
 }
+
+func TestHandlerTriggerSIM2(t *testing.T) {
+
+	var eventDataReq eventData
+	scannerErr := json.Unmarshal(testevent, &eventDataReq)
+	assert.NoError(t, scannerErr)
+	eventDataReq.Address.ParcelAddress = ""
+	aws_Client := new(mocks.IAWSClient)
+	http_Client := new(mocks.MockHTTPClient)
+	mock_auth_client := new(mocks.AuthTokenInterface)
+	mock_auth_client.Mock.On("AddAuthorizationTokenHeader", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	auth_client = mock_auth_client
+	http_Client.Mock.On("Post").Return(&http.Response{
+
+		Body: ioutil.NopCloser(bytes.NewBufferString(string(`{
+			"data": {
+			  "parcels": [
+				{
+				  "state": "NY",
+				  "zip": "14625",
+				  "id": "9a3a3f3b-8ba1-468b-8102-3b3e6ee5d8c1",
+				  "_detectedBuildingCount": {
+                    "marker": "2019-08-29",
+                    "value": 1
+                   },
+				  "structures": [
+					{
+					  "_outline": {
+						"marker": "2019-08-29"
+					  },
+					  "id": "5085a802-89fa-48a8-8c3c-bd8480f0378a"
+					}
+				  ],
+				  "geocoder": {
+					  "lat": 43.172988,
+					  "lon": -77.501957
+				  },
+				  "address": "23 HAVENSHIRE RD",
+				  "city": "ROCHESTER"
+				}
+			  ]
+			}
+		  }`))),
+		StatusCode: http.StatusOK,
+	}, nil)
+	http_Client.On("Get").Return(&http.Response{
+
+		Body: ioutil.NopCloser(bytes.NewBufferString(string(`{
+			"address": "31 Havenshire Rd, Rochester, NY 14625, United States",
+			"geocoder": "bing",
+			"input": {
+				"lat": 43.172733,
+				"lon": -77.501619,
+				"query": "lat=43.172733&lon=-77.501619&parcelID=true"
+			},
+			"lat": 43.172704,
+			"lon": -77.501657,
+			"parcelID": "c7a80489-1693-427e-b1df-869cf985e063",
+			"status": {
+				"code": 1104,
+				"message": "reverse geocoding successful"
+			}
+		}`))),
+		StatusCode: http.StatusOK,
+	}, nil)
+	commonHandler.AwsClient = aws_Client
+	commonHandler.HttpClient = http_Client
+	commonHandler.Secrets = map[string]interface{}{
+		"ClientID":     "id",
+		"ClientSecret": "secret"}
+	expectedResp := eventResponse{
+		Address:    "31 Havenshire Rd, Rochester, NY 14625, United States",
+		Latitude:   43.172988,
+		Longitude:  -77.501957,
+		TriggerSIM: true,
+		ParcelID:   "9a3a3f3b-8ba1-468b-8102-3b3e6ee5d8c1",
+		Message:    NoStructureMessage,
+	}
+	resp, err := notificationWrapper(context.Background(), eventDataReq)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedResp, resp)
+
+}
+
+func TestHandlerGeocodingError503(t *testing.T) {
+
+	var eventDataReq eventData
+	scannerErr := json.Unmarshal(testevent, &eventDataReq)
+	assert.NoError(t, scannerErr)
+	eventDataReq.Address.ParcelAddress = ""
+	aws_Client := new(mocks.IAWSClient)
+	http_Client := new(mocks.MockHTTPClient)
+	mock_auth_client := new(mocks.AuthTokenInterface)
+	slackClient := new(mocks.ISlackClient)
+	slackClient.On("SendErrorMessage", 4051, "", "", "querypdw", "querypdw", "{\"message\":\"503 status code received\",\"messageCode\":4051}", map[string]string(nil)).Return(nil)
+	mock_auth_client.Mock.On("AddAuthorizationTokenHeader", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	auth_client = mock_auth_client
+	http_Client.Mock.On("Post").Return(&http.Response{
+
+		Body: ioutil.NopCloser(bytes.NewBufferString(string(`{
+			"data": {
+			  "parcels": [
+				{
+				  "state": "NY",
+				  "zip": "14625",
+				  "id": "9a3a3f3b-8ba1-468b-8102-3b3e6ee5d8c1",
+				  "_detectedBuildingCount": {
+                    "marker": "2019-08-29",
+                    "value": 1
+                   },
+				  "structures": [
+					{
+					  "_outline": {
+						"marker": "2019-08-29"
+					  },
+					  "id": "5085a802-89fa-48a8-8c3c-bd8480f0378a"
+					}
+				  ],
+				  "geocoder": {
+					  "lat": 43.172988,
+					  "lon": -77.501957
+				  },
+				  "address": "23 HAVENSHIRE RD",
+				  "city": "ROCHESTER"
+				}
+			  ]
+			}
+		  }`))),
+		StatusCode: http.StatusOK,
+	}, nil)
+	http_Client.On("Get").Return(&http.Response{
+		StatusCode: 503,
+	}, nil)
+	commonHandler.AwsClient = aws_Client
+	commonHandler.HttpClient = http_Client
+	commonHandler.SlackClient = slackClient
+	commonHandler.Secrets = map[string]interface{}{
+		"ClientID":     "id",
+		"ClientSecret": "secret"}
+
+	_, err := notificationWrapper(context.Background(), eventDataReq)
+	assert.Error(t, err)
+	assert.Equal(t, error_handler.NewRetriableError(4051, "{\"message\":\"503 status code received\",\"messageCode\":4051}"), err)
+
+}
+
+func TestHandlerGeocodingErrorGetError(t *testing.T) {
+
+	var eventDataReq eventData
+	scannerErr := json.Unmarshal(testevent, &eventDataReq)
+	assert.NoError(t, scannerErr)
+	eventDataReq.Address.ParcelAddress = ""
+	aws_Client := new(mocks.IAWSClient)
+	http_Client := new(mocks.MockHTTPClient)
+	mock_auth_client := new(mocks.AuthTokenInterface)
+	slackClient := new(mocks.ISlackClient)
+	slackClient.On("SendErrorMessage", 4042, "", "", "querypdw", "querypdw", "{\"message\":\"error calling EGS : some error\",\"messageCode\":4042}", map[string]string(nil)).Return(nil)
+	mock_auth_client.Mock.On("AddAuthorizationTokenHeader", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	auth_client = mock_auth_client
+	http_Client.Mock.On("Post").Return(&http.Response{
+
+		Body: ioutil.NopCloser(bytes.NewBufferString(string(`{
+			"data": {
+			  "parcels": [
+				{
+				  "state": "NY",
+				  "zip": "14625",
+				  "id": "9a3a3f3b-8ba1-468b-8102-3b3e6ee5d8c1",
+				  "_detectedBuildingCount": {
+                    "marker": "2019-08-29",
+                    "value": 1
+                   },
+				  "structures": [
+					{
+					  "_outline": {
+						"marker": "2019-08-29"
+					  },
+					  "id": "5085a802-89fa-48a8-8c3c-bd8480f0378a"
+					}
+				  ],
+				  "geocoder": {
+					  "lat": 43.172988,
+					  "lon": -77.501957
+				  },
+				  "address": "23 HAVENSHIRE RD",
+				  "city": "ROCHESTER"
+				}
+			  ]
+			}
+		  }`))),
+		StatusCode: http.StatusOK,
+	}, nil)
+	http_Client.On("Get").Return(&http.Response{
+		StatusCode: 503,
+	}, errors.New("some error"))
+	commonHandler.AwsClient = aws_Client
+	commonHandler.HttpClient = http_Client
+	commonHandler.SlackClient = slackClient
+	commonHandler.Secrets = map[string]interface{}{
+		"ClientID":     "id",
+		"ClientSecret": "secret"}
+
+	_, err := notificationWrapper(context.Background(), eventDataReq)
+	assert.Error(t, err)
+	assert.Equal(t, error_handler.NewServiceError(4042, "{\"message\":\"error calling EGS : some error\",\"messageCode\":4042}"), err)
+
+}
+
+func TestHandlerGeocodingError400(t *testing.T) {
+
+	var eventDataReq eventData
+	scannerErr := json.Unmarshal(testevent, &eventDataReq)
+	assert.NoError(t, scannerErr)
+	eventDataReq.Address.ParcelAddress = ""
+	aws_Client := new(mocks.IAWSClient)
+	http_Client := new(mocks.MockHTTPClient)
+	mock_auth_client := new(mocks.AuthTokenInterface)
+	slackClient := new(mocks.ISlackClient)
+	slackClient.On("SendErrorMessage", 4052, "", "", "querypdw", "querypdw", "{\"message\":\"received invalid http status code: 400\",\"messageCode\":4052}", map[string]string(nil)).Return(nil)
+	mock_auth_client.Mock.On("AddAuthorizationTokenHeader", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	auth_client = mock_auth_client
+	http_Client.Mock.On("Post").Return(&http.Response{
+
+		Body: ioutil.NopCloser(bytes.NewBufferString(string(`{
+			"data": {
+			  "parcels": [
+				{
+				  "state": "NY",
+				  "zip": "14625",
+				  "id": "9a3a3f3b-8ba1-468b-8102-3b3e6ee5d8c1",
+				  "_detectedBuildingCount": {
+                    "marker": "2019-08-29",
+                    "value": 1
+                   },
+				  "structures": [
+					{
+					  "_outline": {
+						"marker": "2019-08-29"
+					  },
+					  "id": "5085a802-89fa-48a8-8c3c-bd8480f0378a"
+					}
+				  ],
+				  "geocoder": {
+					  "lat": 43.172988,
+					  "lon": -77.501957
+				  },
+				  "address": "23 HAVENSHIRE RD",
+				  "city": "ROCHESTER"
+				}
+			  ]
+			}
+		  }`))),
+		StatusCode: http.StatusOK,
+	}, nil)
+	http_Client.On("Get").Return(&http.Response{
+		StatusCode: 400,
+	}, nil)
+	commonHandler.AwsClient = aws_Client
+	commonHandler.HttpClient = http_Client
+	commonHandler.SlackClient = slackClient
+	commonHandler.Secrets = map[string]interface{}{
+		"ClientID":     "id",
+		"ClientSecret": "secret"}
+
+	_, err := notificationWrapper(context.Background(), eventDataReq)
+	assert.Error(t, err)
+	assert.Equal(t, error_handler.NewServiceError(4052, "{\"message\":\"received invalid http status code: 400\",\"messageCode\":4052}"), err)
+
+}
+
 func TestHandlerParcelDoesnotExist(t *testing.T) {
 
 	var eventDataReq eventData
